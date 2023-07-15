3784ba8419fc0d6620fb6236f4fd68893857f3c9
Sina <sadeghesfahani.sina@gmail.com>
2023-07-15 13:37:28 +0330

    Fix models

--- user/models.py
+++ user/models.py
@@ -6,32 +6,14 @@ from user.enums import BuildingFramework, BuildingOccupancy
 
 # Create your models here.
 
-class Framework(models.Model):
-    name = models.CharField(max_length=120)
-    universal_id = models.SmallIntegerField(choices=[(e.value, e.name) for e in BuildingFramework], unique=True)
-    description = models.TextField(max_length=120)
-    image = models.ImageField(upload_to='frameworks', null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Occupancy(models.Model):
-    name = models.CharField(max_length=120)
-    universal_id = models.SmallIntegerField(choices=[(e.value, e.name) for e in BuildingOccupancy], unique=True)
-    description = models.TextField(max_length=120)
-    image = models.ImageField(upload_to='occupancies', null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
 
 class User(AbstractUser):
     phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(blank=False, null=False, unique=True)
+    email = models.EmailField(blank=True, null=True)
     token = models.CharField(max_length=255, blank=True)
     is_superuser = models.BooleanField(default=False)
     building_construction_year = models.CharField(max_length=4, blank=True)
-    framework_x = models.ForeignKey(Framework, on_delete=models.CASCADE, related_name="framework_x")
-    framework_y = models.ForeignKey(Framework, on_delete=models.CASCADE, related_name="framework_y")
-    occupancy = models.ForeignKey(Occupancy, on_delete=models.CASCADE, related_name="occupancy")
+    framework_x = models.IntegerField(blank=True, choices=[(e.value, e.name) for e in BuildingFramework])
+    framework_y = models.IntegerField(blank=True, choices=[(e.value, e.name) for e in BuildingFramework])
+    occupancy = models.IntegerField(blank=True, choices=[(e.value, e.name) for e in BuildingOccupancy])
