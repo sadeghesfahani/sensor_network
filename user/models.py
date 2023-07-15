2cef0d6869ddb9f3b67726decb75ba925609fcbb
Sina <sadeghesfahani.sina@gmail.com>
2023-07-15 18:43:11 +0330

    Fix not null contrain

--- user/models.py
+++ user/models.py
@@ -7,13 +7,12 @@ from user.enums import BuildingFramework, BuildingOccupancy
 # Create your models here.
 
 
-
 class User(AbstractUser):
     phone = models.CharField(max_length=20, blank=True)
     email = models.EmailField(blank=True, null=True)
     token = models.CharField(max_length=255, blank=True)
     is_superuser = models.BooleanField(default=False)
     building_construction_year = models.CharField(max_length=4, blank=True)
-    framework_x = models.IntegerField(blank=True, choices=[(e.value, e.name) for e in BuildingFramework])
-    framework_y = models.IntegerField(blank=True, choices=[(e.value, e.name) for e in BuildingFramework])
-    occupancy = models.IntegerField(blank=True, choices=[(e.value, e.name) for e in BuildingOccupancy])
+    framework_x = models.IntegerField(blank=True, null=True, choices=[(e.value, e.name) for e in BuildingFramework])
+    framework_y = models.IntegerField(blank=True, null=True, choices=[(e.value, e.name) for e in BuildingFramework])
+    occupancy = models.IntegerField(blank=True, null=True, choices=[(e.value, e.name) for e in BuildingOccupancy])
